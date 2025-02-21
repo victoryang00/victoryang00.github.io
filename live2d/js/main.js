4f98eccc2c39aa174e26bc0a1ede0eaa6bf0914b
victoryang00 <victoryang00@ucsc.edu>
2025-02-20 17:09:41 -0800

    fix error

--- live2d/js/main.js
+++ live2d/js/main.js
@@ -6,13 +6,13 @@ $(function () {
     $('#Fixed').click(() => {
         window.switchAlwaysOnTop()
     })
-    window.ws.onmessage = function (event) {
-        const msgObj = JSON.parse(event.data)
-        if (msgObj.type === 'message') {
-            const duration = msgObj.duration ? msgObj.duration : 2000
-            window.receiveMsg(msgObj.message, duration)
-        } else if (msgObj.type === 'death') {
-            window.close()
-        }
-    };
+    // window.ws.onmessage = function (event) {
+    //     const msgObj = JSON.parse(event.data)
+    //     if (msgObj.type === 'message') {
+    //         const duration = msgObj.duration ? msgObj.duration : 2000
+    //         window.receiveMsg(msgObj.message, duration)
+    //     } else if (msgObj.type === 'death') {
+    //         window.close()
+    //     }
+    // };
 })
